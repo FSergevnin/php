db762487924449b1e82fcd3e353006973fdc8f74
FSergevnin <sergevnin@gmail.com>
2019-07-14 22:47:12 +0300

    сделана загрузка галереи по ссылке на директорию с картинками

--- lesson-4/public/gallery.php
+++ lesson-4/public/gallery.php
@@ -2,19 +2,9 @@
 
 require_once __DIR__ . '/../config/config.php';
 
-// $variables = [
-// 	'title' => 'Главная страница',
-// 	'h1' => 'Добро пожаловать',
-// 	'content' => '<div class="privet"><img src="/img/1.jpg"></div>'
-// ];
-// $templateName = TEMPLATES_DIR . 'index.tpl';
-
-
-// echo render($templateName, $variables);
-
 foreach (scandir(WWW_DIR . IMG_DIR) as $fileName) {
 		if (substr($fileName, -4, 4) == '.jpg' || substr($fileName, -4, 4) == '.png' || substr($fileName, -4, 4) == '.gif') {
-			$strGallary .= '<img class = "miniImg" src="' . IMG_DIR . $fileName . '" alt = "' . $fileName . '" width="350">';
+			$strGallary .= '<a href="' . IMG_DIR . $fileName . '" target="_blank"><img class = "miniImg" src="' . IMG_DIR . $fileName . '" alt = "' . $fileName . '" width="350"></a>';
 		}
 }
 
