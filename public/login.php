013619b58849cebe77867be719259b82b5baa5c5
FSergevnin <sergevnin@gmail.com>
2019-07-23 23:20:16 +0300

    сделана страничка авторизации

--- public/login.php
+++ public/login.php
@@ -1,10 +1,40 @@
 <?php
 
+require_once '../config/config.php';
+
+
+$login = $_POST['login'] ?? '';
+$password = $_POST['password'] ?? '';
+
+
+if ($login && $password) {
+	//преобразуем пароль в хэш
+	$passwordXeh = md5($password);
+	//получаем пользователя из базы по логин-паролю
+	$sql = "SELECT * FROM `users` WHERE `login` = '$login' AND `password` = '$passwordXeh'";
+
+    $user = show($sql);
+
+	//если пользователь найден. Записываем его в сессию
+	if($user) {
+		$_SESSION['login'] = $user;
+
+        $message = 'Привет ' . $_SESSION['login']['login'];
+	} else {
+		$message = 'Неверная пара логин-пароль';
+	}
+}
+
 
 
  ?>
 
+<div><?= $message ?></div>
 <form method="post">
-    <input type="text" name="login" value="<?= $login ?>">
-    <input type="password" name="pasword" value="<?= $pasword ?>">
+    <pre>
+        <input type="text" name="login" value="<?= $login ?>">
+        <input type="password" name="password" value="<?= $password ?>">
+        <input type="submit" name="aut" value="Войти">
+    </pre>
+
 </form>
