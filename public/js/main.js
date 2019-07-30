195c85102edb06252df95368fbd5110899254fbc
FSergevnin <sergevnin@gmail.com>
2019-07-30 23:00:55 +0300

    join js and jquery

---
+++ public/js/main.js
@@ -0,0 +1,110 @@
+// $.post({
+// 	url: '/action.php?id=1',
+// 	data: {a: 1}
+// });
+
+//Функция AJAX авторизации
+function login() {
+	//Получаем input'ы логина и пароля
+	const $login_input = $('[name="login"]');
+	const $password_input = $('[name="password"]');
+
+	//Получаем значение login и password
+	const login = $login_input.val();
+	const password = $password_input.val();
+
+	//Инициализируем поле для сообщений
+	const $message_field = $('.message');
+
+	//Вызываем функцию jQuery AJAX с методом POST
+	//Передаем туда url где будет обрабатваться API
+	//И data которое будет помещена в $_POST
+	//success - вызывается при успешном ответе от сервера
+	$.post({
+		url: '/api.php',
+		data: {
+			apiMethod: 'login',
+			postData: {
+				login: login,
+				password: password
+			}
+		},
+		success: function (data) {
+			//data приходят те данные, который прислал на сервер
+
+
+			//Вариант с json
+			// if(data.error) {
+			// 	$message_field.text(data.error_text);
+			// } else {
+			// 	location.reload();
+			// }
+
+			//Вариан без json
+			if (data === 'OK') {
+				location.reload();
+			} else {
+				$message_field.text(data);
+			}
+		}
+	});
+}
+
+function addToCart(id) {
+	$.post({
+		url: '/api.php',
+		data: {
+			apiMethod: 'addToCart',
+			postData: {
+				id: id
+			}
+		},
+		success: function (data) {
+			if(data === 'OK') {
+				alert('Товар добавлен в корзину');
+			} else {
+				alert(data);
+			}
+		}
+	})
+}
+
+function removeFromCart(id) {
+	$.post({
+		url: '/api.php',
+		data: {
+			apiMethod: 'removeFromCart',
+			postData: {
+				id: id
+			}
+		},
+		success: function (data) {
+			if(data === 'OK') {
+				$('[data-id="' + id + '"]').remove();
+			} else {
+				alert(data);
+			}
+		}
+	})
+}
+
+function changeOrderStatus(orderId, newStatus)
+{
+	$.post({
+		url: '/api.php',
+		data: {
+			apiMethod: 'changeOrderStatus',
+			postData: {
+				orderId: orderId,
+				newStatus: newStatus
+			}
+		},
+		success: function (data) {
+			if (data === 'OK') {
+				location.reload();
+			} else {
+				alert(data);
+			}
+		}
+	});
+}
