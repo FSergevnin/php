96fcbe5f2da49f21213862822fa8113eaeabc7bb
FSergevnin <sergevnin@gmail.com>
2019-08-03 22:31:37 +0300

    сделан api

---
+++ public/api.php
@@ -0,0 +1,144 @@
+<?php
+
+/*
+ * Файл работы API
+ * Файл ожидает что в _POST придет apiMethod с задачей, которую нужно выполнить
+ * И (при необходимости) postData с информацией, необходимой для этой задачи
+ *
+ */
+
+/*
+ * Комментарий по json
+ * Если использовать header('Content-Type: application/json');
+ * То весь текст на странице попытается преобразоваться в json.
+ * Следовательно нельзя будет увидеть ошибки, которые вам покажет php,
+ * поэтому задает заголовок передаем в последний момент
+ *
+ * Если до этого были ошибки на php заголовок задать не получится
+ *
+ */
+
+require_once '../config/config.php';
+
+//Функция вывода ошибки
+function error($error_text)
+{
+	//Вариант с json
+//	header('Content-Type: application/json');
+//	echo json_encode([
+//		'error' => true,
+//		'error_text' => $error_text,
+//		'data' => null
+//	]);
+//	exit();
+
+	//Вариант без json
+	echo "Ошибка: $error_text";
+	exit();
+}
+
+//Функция успешного ответа
+function success($data = true)
+{
+	//Вариант с json
+//	header('Content-Type: application/json');
+//	echo json_encode([
+//		'error' => false,
+//		'error_text' => null,
+//		'data' => $data
+//	]);
+//	exit();
+
+	//Вариант без json
+	echo "OK";
+	exit();
+}
+
+//Если на api не передан apiMethod вызываем ошибку
+if (empty($_POST['apiMethod'])) {
+	error('Не передан apiMethod');
+}
+
+
+//Обработка метода login
+if ($_POST['apiMethod'] === 'login') {
+
+	//Получаем логин и пароль из postData
+	$login = $_POST['postData']['login'] ?? '';
+	$password = $_POST['postData']['password'] ?? '';
+
+	//Если нет логина или пароля вызываем ошибку
+	if (!$login || !$password) {
+		error('Логин или пароль не переданы');
+	}
+
+	//приводим пароль к тому же виду, как он хранится в базе
+	$password = md5($password);
+
+	//генерируем запрос и пытаемся найти пользователя
+	$sql = "SELECT * FROM `users` WHERE `login` = '$login' AND `password` = '$password'";
+	$user = show($sql);
+
+	//Если пользователь найден, записываем информацию о пользователе в сессию,
+	//что бы к ней можно было обратиться с любой страницы
+	//Если пользователь не найден, возвращаем ошибку
+	if ($user) {
+		$_SESSION['login'] = $user;
+		success();
+	} else {
+		error('Неверная пара логин-пароль');
+	}
+}
+
+//Обработка метода addToBasket
+if($_POST['apiMethod'] === 'addToBasket') {
+	$id = $_POST['postData']['id'] ?? 0;
+	if(!$id) {
+		error('ID не передан');
+	}
+
+	//Получаем данные корзины
+	$basket = $_COOKIE['basket'] ?? [];
+
+	//если в корзине товара еще нет, то получаем 0
+	$count = $basket[$id] ?? 0;
+	//увеличиваем количество в корзине
+	$count++;
+
+	//устанавливаем новое куки
+	setcookie("basket[$id]", $count);
+	success();
+}
+
+//Обработка метода removeFromBasket
+if ($_POST['apiMethod'] === 'removeFromBasket') {
+	$id = $_POST['postData']['id'] ?? 0;
+	if(!$id) {
+		error('ID не передан');
+	}
+
+	//удаляем товар из корзины
+	setcookie("basket[$id]", null);
+	success();
+}
+
+if ($_POST['apiMethod'] === 'changeOrderStatus') {
+	$orderId = $_POST['postData']['orderId'];
+	$newStatus = $_POST['postData']['newStatus'];
+
+	if (!$orderId || !$newStatus) {
+		error('Недостаточно данных');
+	}
+
+
+	if (changeOrderStatus($orderId, $newStatus)) {
+		success();
+	} else {
+		error('Произошла ошибка');
+	}
+}
+
+
+
+
+error('Неизвестный метод');
